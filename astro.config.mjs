6186f230f005c9c80d796bf3b843139572d8d13e
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-06-16 12:34:51 +0600

    Add SEO and sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react({ experimentalReactChildren: true })],
+  site: 'https://www.sohom829.xyz/',
+  integrations: [
+    tailwind(),
+    react({
+      experimentalReactChildren: true,
+    }),
+    sitemap(),
+  ],
 });
